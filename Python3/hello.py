d74bd5849ce09f550cecedef0b7f31e94952c974
jesusrlv <jesusrlvrojo@gmail.com>
2024-06-25 18:41:42 -0600

    inicio python

---
+++ Python3/hello.py
@@ -0,0 +1,12 @@
+# se imprimirá Hola mundo
+print("Hola mundo, este es el código inicial de Pyhton, de aquí inicia lo mero bueno")
+
+#suma
+
+print(3 + 2)
+
+print(type(3+4)) #integer
+print(type(1.5)) #float
+print(type(True)) #boolean
+print(type(1 + 9j)) #boolean
+print(type("hola mundo")) #integer
