9867b395b981dcf234ba1bcc15d904107f915c45
jersusrlv <jesusrlvrojo@gmail.com>
2024-07-02 23:54:12 -0600

    operadores

---
+++ 02_operadores.py
@@ -0,0 +1,12 @@
+#operadores
+
+print(3 + 6)
+print(3 - 6)
+print(3 * 6)
+print(3 / 6)
+
+print(3 % 6) #divide y deja el resto o más bien lo imprime
+print(3 // 6) #se divide y da un nùmero entero
+print(2 ** 3) #exponencial al cubo o a lo que se le ponga
+print("hola" + "python") #concatena de otra manera
+print("hola " * 5) #multiplica por 5 la palabra hola
