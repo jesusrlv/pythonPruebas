deaf022c77f57c3468ad13faeb069dafc3b366ea
jesusrlv <jesusrlvrojo@gmail.com>
2024-07-15 09:48:51 -0600

    diccionarios

---
+++ Python3/07_dicts.py
@@ -0,0 +1,21 @@
+### Dictionaries ###
+
+my_dict = dict()
+my_other_dict = {}
+
+print(type(my_dict))
+print(type(my_other_dict))
+
+my_other_dict = {"nombre": "jesusrlv","apellido":"leaños", "edad": 42}
+
+my_dict ={
+    "nombre": "rodolfo",
+    "apellido": "leaños",
+    "edad": 43, 
+    "lenguajes": {"Python", "Swfit", "Dart"},
+    1: 1.95
+} 
+print(my_dict)
+print(my_other_dict)
+
+print(len(my_dict))
