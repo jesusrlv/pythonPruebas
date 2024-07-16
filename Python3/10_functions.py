9b032ed64c0e337bf7b5b0b43086706e999d3dea
jesusrlv <jesusrlvrojo@gmail.com>
2024-07-16 13:18:05 -0600

    functions

---
+++ Python3/10_functions.py
@@ -0,0 +1,21 @@
+### Functions ###
+
+def my_function ():
+    print("Hola mundo desde una función")
+
+my_function()
+my_function()
+my_function()
+
+def sum_two_values ():
+    num1 = 5
+    num2 = 10
+    return num1 + num2
+
+print(sum_two_values())
+
+def sum_and_multiply_two_values (num1, num2):
+    result = num1 + num2
+    return result * 2
+
+print(sum_and_multiply_two_values(5, 10))
