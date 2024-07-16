5038e433379dbb60d08b4f43e5434edee8fefe9d
jesusrlv <jesusrlvrojo@gmail.com>
2024-07-16 12:26:06 -0600

    termina ciclo for

--- Python3/09_loops.py
+++ Python3/09_loops.py
@@ -25,7 +25,59 @@ print("La ejecución continúa")
 
 #los for son más eficientes que los while, ya que Python puede optimizarlos
 
+print("------------------------------------------------")
+print("lista")
+print("------------------------------------------------")
+
 my_list = [1,2,3,4,5,6,7]
 
 for i in my_list:
     print(i)
+
+print("------------------------------------------------")
+print("tupla")
+print("------------------------------------------------")
+
+my_tuple = (43, 1.95, "jesus", "jesusrlv")
+
+for i in my_tuple:
+    print(i)
+
+print("------------------------------------------------")
+print("set")
+print("------------------------------------------------")
+
+my_set = {"rodolfo","jesusrlv",43}
+
+for i in my_set:
+    print(i)
+
+print("------------------------------------------------")
+print("diccionario sin valores")
+print("------------------------------------------------")
+my_dict = {"nombre": "jesusrlv","apellido":"leaños", "edad": 42}
+
+for i in my_dict:
+    print(i)
+
+print("------------------------------------------------")
+print("diccionario con valores")
+print("------------------------------------------------")
+my_dict = {"nombre": "jesusrlv","apellido":"leaños", "edad": 42}
+
+for i in list(my_dict.values()):
+    print(i)
+
+print("------------------------------------------------")
+print("else")
+print("------------------------------------------------")
+my_dict = {"nombre": "jesusrlv","apellido":"leaños", "edad": 42}
+
+for i in list(my_dict.values()):
+    print(i)
+    if my_dict == "leaños":
+        # print("encontró el 5")
+        continue
+    print("se ejecuta")
+else:
+    print("El bucle se terminó sin encontrar valores")
