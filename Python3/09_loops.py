cd25feabec4e479fa1bb350456bc92f48d328d44
jesusrlv <jesusrlvrojo@gmail.com>
2024-07-16 11:26:38 -0600

    ciclo for

--- Python3/09_loops.py
+++ Python3/09_loops.py
@@ -10,4 +10,22 @@ while my_condition < 10:
 else: #el else es opcional
     print("Se terminó el while por la condición mayor o igual a 10")
 
+
+
+while my_condition < 20:
+    my_condition += 1
+    if my_condition == 15:
+        print("------------------------------------------------")
+        print("mi condición es igual a 15")
+        print("------------------------------------------------")
+        break #rompe el bucle cuando la condición se cumple
+    print(my_condition)
+
 print("La ejecución continúa")
+
+#los for son más eficientes que los while, ya que Python puede optimizarlos
+
+my_list = [1,2,3,4,5,6,7]
+
+for i in my_list:
+    print(i)
