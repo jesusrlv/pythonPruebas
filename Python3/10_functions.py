f37bc21c3139a83568668d163a81a4d1e01b6990
jersusrlv <jesusrlvrojo@gmail.com>
2024-07-16 19:56:07 -0600

    termina funciones

--- Python3/10_functions.py
+++ Python3/10_functions.py
@@ -23,4 +23,18 @@ print(sum_and_multiply_two_values(5, 10))
 def print_name(name,surname):
     print(f"Mi nombre es {name} y me dicen {surname}")
 
-print_name("rodolfo", "jesusrlv")
+print_name(surname="rodolfo", name="jesusrlv")
+
+def print_name_with_default(name,surname,alias="sin alias"):
+    print(f"Mi nombre es {name} y me dicen {surname} y {alias}")
+
+print_name_with_default("rodolfo","leaños")
+print_name_with_default("rodolfo","leaños","rojo")
+
+def print_texts(*texts):
+    for text in texts:
+        print(text.upper())
+
+print_texts("hola","python","jesusrlv")
+print_texts("python","jesusrlv")
+
