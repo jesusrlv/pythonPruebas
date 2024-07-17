4de6061931e0993d35c7477cc22a612e5be50ab8
jersusrlv <jesusrlvrojo@gmail.com>
2024-07-16 21:56:56 -0600

    modulos

---
+++ Python3/13_modules.py
@@ -0,0 +1,19 @@
+### modules ###
+
+import module
+
+module.suma(5, 3, 1)
+module.restar("Hello world!!!")
+
+import math
+
+
+print(math.pi)
+
+val = math.pow(2,8)
+
+print("el valor de 2 elevado a la 8 =", val)
+
+from math import pi as PI_Value
+
+print("el valor de pi es",PI_Value)
---
+++ Python3/module.py
@@ -0,0 +1,7 @@
+
+
+def suma(numeroUno, numeroDos, numeroTres):
+    print(numeroUno+numeroDos+numeroTres)
+
+def restar(value):
+    print(value)
