56f2b204c62f3545d9faf8895dd45f03de718a12
jesusrlv <jesusrlvrojo@gmail.com>
2024-07-18 10:06:37 -0600

    json python

--- Python3_Parte_2/06_file_handling.py
+++ Python3_Parte_2/06_file_handling.py
@@ -20,3 +20,22 @@ print(txt_file.readline())
 txt_file.close()
 
 #os.remove("Python3_Parte_2/myFile.txt") #elimina el file
+
+# .json file
+
+import json
+
+json_file = open("Python3_Parte_2/myFile.json", "w+")
+
+json_test = {
+    "nombre": "jesus",
+    "surname": "rlv",
+    "age": 42,
+    "language": ["python", "swift", "php", "js"],
+    "website": "http://dev.net"
+    }
+json.dump(json_test, json_file, indent = 8) #agrega el mapa de datos, luego pide el documento y al final la identanción que se da por espacios cada número
+json_file.close() # se tiene que cerrar para poderlo leer en la consola
+with open("Python3_Parte_2/myFile.json") as my_other_file: #se genera este para lerlo
+    for line in my_other_file.readlines():
+        print(line)
