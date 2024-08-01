a3353d0a9c30609d74f84a22d5334e5e3404a27a
jesusrlv <jesusrlvrojo@gmail.com>
2024-08-01 03:36:05 -0600

    virtual y uso de fastAPI

---
+++ Backend/FastAPI/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return "hola mundo fastAPI"
+
+@app.get("/url")
+async def root():
+    return {"url_curso":"http://google.com",
+            "nombre":"jesus",
+            "apellido":"leaños",
+            "nickname":"jesusrlv"
+            }
