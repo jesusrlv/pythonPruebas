70343773080ff6763bae3891d8c191871e8704e4
jesusrlv <jesusrlvrojo@gmail.com>
2024-08-01 01:31:04 -0600

    fastApi inicio

--- Backend/00_type_hints.py
+++ Backend/00_type_hints.py
@@ -1,1 +1,22 @@
 ## type hints ##
+
+## web de fastApi 
+# https://fastapi.tiangolo.com/es/python-types/
+
+my_variable_string = 'La variable es string'
+print(my_variable_string)
+print(type(my_variable_string))
+
+my_variable_string = 5
+print(my_variable_string)
+print(type(my_variable_string))
+
+my_typed_variable: str = "my typed variable"
+#my_typed_variable.
+print(my_typed_variable)
+print(type(my_typed_variable))
+
+
+my_typed_variable: str = 5
+print(my_typed_variable)
+print(type(my_typed_variable))
