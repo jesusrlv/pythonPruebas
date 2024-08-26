0b5f1112de4998dec73d4329b2a4b18617fa1084
jesusrlv <jesusrlvrojo@gmail.com>
2024-08-26 00:07:57 -0600

    terminado el GET, PUT, DELETE

--- Backend/FastAPI/users.py
+++ Backend/FastAPI/users.py
@@ -81,4 +81,37 @@ def search_user(id:int):
      try:
         return list(users)[0]
      except:
-         return {"error":"no se ha encontrado el usuario"}
+         return {"error":"el usuario ya existe"}
+     
+@app.post("/user/")
+async def user(user: User): #se trae los datos de User con los tipados
+    if type(search_user(user.id)) == User:
+        return {"error":"no se ha encontrado el usuario"}
+    else:
+
+        users_list.append(user)
+        return user
+
+@app.put("/user/")
+async def user(user: User):
+
+    found = False
+
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+    if not found:
+        return {"error":"no se ha actualizado el usuario"}
+    else:
+        return user
+    
+@app.delete("/user/{id}")
+async def user(id: int):
+    found = False
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == id:
+            del users_list[index]
+            found = True
+    if not found:
+        return {"error":"no se ha eliminado el usuario"}
